2c4f773f55e48a2ac6e7c2f8266b4dcee00eac3e
Carlos Vanegas <cvanegas@carloss-mbp.lan>
2023-08-28 15:38:52 -0500

    Simple button test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Reshape Automation
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://www.reshapeautomation.com/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Future ome of the AutoCanvas ROI Calculator
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Reshape Automation
-        </p>
-        <a
-          className="App-link"
-          href="https://www.reshapeautomation.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Future home of the AutoCanvas ROI Calculator
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
